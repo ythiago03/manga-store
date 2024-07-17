99edd0085731bd9697b9d726ef0ac866d1b0ecd2
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-07-17 20:14:32 -0300

    Adiciona responsividade aos cards

--- src/components/SliderCards/SliderCards.tsx
+++ src/components/SliderCards/SliderCards.tsx
@@ -3,6 +3,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import './SliderCards.css';
 import Card from '../Card/Card';
+import { useEffect, useState } from 'react';
 
 interface CardProp {
   id: number,
@@ -13,11 +14,40 @@ interface CardProp {
 }
 
 const SliderCards = ({cards}: {cards: CardProp[]}) => {
-  
+  const [slidesPerView, setSlidesPerView] = useState<number>(4);
+
+  const getWindowDimensions = () => {
+    const { innerWidth } = window;
+
+    if(innerWidth >= 1050){
+      return 4;
+    }
+    if(innerWidth <= 1050 && innerWidth >= 820){
+      return 3;
+    }
+    if(innerWidth <= 820 && innerWidth >= 580 ){
+      return 2;
+    }
+    return 1;
+  }; 
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSlidesPerView(getWindowDimensions());
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    setSlidesPerView(getWindowDimensions());
+  },[]);
+
   return (
     <Swiper 
       className="sliderCard-container"
-      slidesPerView={4}
+      slidesPerView={slidesPerView}
       navigation
     >
       {cards.map((card: CardProp) => {   
